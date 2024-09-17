d611296f819e06cebaf623f8dadccda0aa4ec957
Macken <passap12rus854565@gmail.com>
2024-09-17 20:44:00 +0300

    5.8. Домашнее задание - Управление потоком

---
+++ 5-languages/app.js
@@ -0,0 +1,17 @@
+const userLang = prompt('Select language: en, ru, de, fr ');
+switch(userLang) {
+    case 'en':
+        console.log('Hello!');
+        break;
+    case 'ru':
+        console.log('Добрый день!');
+        break;
+    case 'de':
+        console.log('Guten Tag!');
+        break;
+    case 'fr':
+        console.log('Bonjour!');
+        break;
+    default:
+        console.log('Please select your language!');
+}
